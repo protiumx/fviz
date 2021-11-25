29af09a126928aa7d0d4aa2ac350b875a166e6cd
Brian Mayo <ioprotium@gmail.com>
2021-11-25 19:59:35 +0100

    feat: added Cesium example

--- packages/fviz-ui/src/App.tsx
+++ packages/fviz-ui/src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  Cesium.Ion.defaultAccessToken = process.env.REACT_APP_CESIUM_TOKEN as string;
+
+  useEffect(() => {
+    const viewer = new Cesium.Viewer(document.getElementById("viewer"));
+
+    viewer.entities.add({
+      name: "tokyo",
+      description: "test",
+      position: Cesium.Cartesian3.fromDegrees(139.767052, 35.681167, 100),
+      point: { pixelSize: 10 },
+    });
+  }, []);
+  return <div id="viewer"></div>;
 }
 
 export default App;
