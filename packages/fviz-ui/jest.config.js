debaacf00be300be41a9e40e8699e260c1339f15
Brian Mayo <ioprotium@gmail.com>
2021-11-25 22:52:06 +0100

    chore: upgraded jest and added axios

--- packages/fviz-ui/jest.config.js
+++ packages/fviz-ui/jest.config.js
@@ -1,12 +1,7 @@
 process.env.TZ = "UTC";
 
 module.exports = {
-  collectCoverageFrom: [
-    "src/**/*.{ts,tsx}",
-    "!src/index.tsx",
-    "!src/App.tsx",
-    "!src/setupTests.ts",
-  ],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/index.tsx", "!src/App.tsx"],
   coverageDirectory: "coverage",
   coverageReporters: process.env.GITHUB_ACTIONS
     ? ["lcovonly", "text"]
@@ -21,16 +16,21 @@ module.exports = {
   },
   globals: {
     "ts-jest": {
+      babel: true,
       tsconfig: "tsconfig.json",
     },
   },
-  moduleFileExtensions: ["json", "ts", "tsx", "js", "jsx"],
-  setupFilesAfterEnv: ["./jest.setup.ts"],
-  testEnvironment: require.resolve(`jest-environment-node`),
-  testMatch: ["**/**/test/**/*.(spec|test).ts?(x)"],
-  transform: {
-    "^.+\\.tsx?$": require.resolve("ts-jest"),
+  moduleNameMapper: {
+    "^$/(.*)$": "<rootDir>/src/$1",
   },
-  transformIgnorePatterns: ["/.pnp.cjs$"],
+  moduleFileExtensions: ["json", "ts", "tsx", "js", "jsx"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  testEnvironment: "node",
+  testMatch: ["**/*.(spec|test).ts?(x)"],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["/.pnp.cjs$", "/node_modules/", "/dist"],
   verbose: true,
 };
