ad2fcdae849a9fbad358d38f7d21d2c9ba060940
Brian Mayo <ioprotium@gmail.com>
2021-11-25 18:15:56 +0100

    refactor: load known devices in memory

--- packages/fviz-server/src/main.rs
+++ packages/fviz-server/src/main.rs
@@ -11,18 +11,35 @@ mod transport;
 
 extern crate dotenv;
 
+/// For testing purposes. Devices should be loaded from a DB and verified.
+static DEVICES: &'static [&'static str] = &["63bbbb82-c6ce-430f-ac19-f6479cee4ab7"];
+
 #[tokio::main]
 async fn main() {
   let path = std::path::Path::new("../.env");
   dotenv::from_path(path).ok();
   let clients: ws::Clients = Arc::new(RwLock::new(HashMap::new()));
-
+  load_devices(&clients).await;
   let addr = std::env::var("HOST_PORT")
     .ok()
     .and_then(|string| SocketAddr::from_str(&string).ok())
     // Default to port 9000
     .unwrap_or_else(|| SocketAddr::from_str("127.0.0.1:9000").unwrap());
   run_server(addr, &clients).await;
+}
+
+async fn load_devices(clients: &ws::Clients) {
+  for item in DEVICES.into_iter().enumerate() {
+    let (i, duid): (usize, &&str) = item;
+    clients.write().await.insert(
+      duid.to_string(),
+      ws::Client {
+        client: format!("FVIZ-[{}]", i),
+        sender: None,
+      },
+    );
+  }
+  println!("Devices loaded");
 }
 
 async fn run_server(addr: SocketAddr, clients: &ws::Clients) {
@@ -47,7 +64,7 @@ async fn run_server(addr: SocketAddr, clients: &ws::Clients) {
     .or(ws_route)
     // enable CORS
     .with(warp::cors().allow_any_origin());
-  println!("Running server at: {}", addr);
+  println!("Server running at: {}", addr);
   warp::serve(routes).run(addr).await;
 }
 
